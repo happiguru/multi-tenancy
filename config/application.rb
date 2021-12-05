683e2ee1f1668efb8b3520b478c011e16d3668db
happiguru <hguruman@gmail.com>
2021-12-05 18:12:42 +0400

    Add subdomain to account

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module MultiTenancy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.hosts = nil
 
     # Configuration for the application, engines, and railties goes here.
     #
